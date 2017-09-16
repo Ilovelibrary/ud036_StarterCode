7dbb9224385eba06103d59a65784a8fbc2da1350
Pu <p5gao@ucsd.edu>
2017-09-16 00:38:19 -0700

    create class, instances, and finsh opening the page

---
+++ entertainment_center.py
@@ -0,0 +1,23 @@
+import media
+
+avator = media.Movie("Avatar",
+	"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY");
+
+fast_and_furious_8 = media.Movie("Fast & Furious 8",
+	"https://upload.wikimedia.org/wikipedia/en/2/2d/The_Fate_of_The_Furious_Theatrical_Poster.jpg",
+	"https://www.youtube.com/watch?v=uisBaTkQAEs");
+
+logan = media.Movie("Logan",
+	"https://upload.wikimedia.org/wikipedia/en/3/37/Logan_2017_poster.jpg",
+	"https://www.youtube.com/watch?v=Div0iP65aZo");
+	
+wonder_woman = media.Movie("Wonder Woman",
+	"https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
+	"https://www.youtube.com/watch?v=VSB4wGIdDwo");
+
+wolf_warrior_2 = media.Movie("Wolf Warrior 2",
+	"https://upload.wikimedia.org/wikipedia/en/1/11/Wolf_Warriors_poster.jpg",
+	"https://www.youtube.com/watch?v=fkqGiPB2D8M");
+
+movies = [avator, fast_and_furious_8, logan, wonder_woman, wolf_warrior_2];
